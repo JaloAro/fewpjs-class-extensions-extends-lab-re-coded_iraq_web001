7f0d6ff96b3670bffbf99e1a9b5d80b2a1fd1333
jalal Arif <jalalharfjalal@gmail.com>
2020-07-02 19:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+class Polygon{
+  constructor(array){
+    this.aside=array
+    
+  }
+  
+  get counterSide(){
+        return this.aside.length
+  }
+  
+  get perimeter(){
+    
+  }
+  
+}
